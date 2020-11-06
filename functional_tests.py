bab93a4c6f5578cb5f8a25727a00b0f878b139fb
for_no_reason <wlwonderful@163.com>
2020-11-06 14:13:28 +0800

    Show entered item on the home page

--- functional_tests.py
+++ functional_tests.py
@@ -21,7 +21,7 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 		
-		inputbox.send_keys('Buy peacock feather')
+		inputbox.send_keys('Buy peacock feathers')
 		inputbox.send_keys(Keys.ENTER)
 
 		time.sleep(3)
@@ -29,7 +29,19 @@ class NewVisitorTest(unittest.TestCase):
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 
-		self.assertTrue(any(row.text == '1: Buy peacock feather' for row in rows))
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
+
+		time.sleep(3)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
 
 		self.fail('Finish the test!')
