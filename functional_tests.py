e780f5d6254432e807de12b7d2e4cb9828ff6568
for_no_reason <wlwonderful@163.com>
2020-11-06 14:18:30 +0800

    Refactor FT by adding tool method

--- functional_tests.py
+++ functional_tests.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.keys import Keys
 import unittest
 import time
 
+
 class NewVisitorTest(unittest.TestCase):
 
 	def setUp(self):
@@ -10,6 +11,14 @@ class NewVisitorTest(unittest.TestCase):
 
 	def tearDown(self):
 		self.browser.quit()
+
+
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+
+		self.assertIn(row_text, [row.text for row in rows])
+
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		self.browser.get('http://localhost:8000')
@@ -26,10 +35,7 @@ class NewVisitorTest(unittest.TestCase):
 
 		time.sleep(3)
 
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Use peacock feathers to make a fly')
@@ -37,12 +43,8 @@ class NewVisitorTest(unittest.TestCase):
 
 		time.sleep(3)
 
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
-
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 		self.fail('Finish the test!')
 
